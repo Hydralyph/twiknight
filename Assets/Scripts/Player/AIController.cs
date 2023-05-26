f462d6b8d815cbaed4b41a9be32453dbad90300e
Hydralyph <acidicmc@gmail.com>
2023-05-26 13:21:35 +1200

    Changed Asset directory for some newly imported assets, added some new scripts for movement, level transitioning, and more.

---
+++ Assets/Scripts/Player/AIController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AIController", menuName = "TwiKnight/AIController")]
+public class AIController : InputController
+{
+    public override bool RetrieveJumpInput()
+    {
+        return true;
+    }
+
+    public override float RetrieveMoveInput()
+    {
+        return 1f;
+    }
+}
