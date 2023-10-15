289bddbb7b6877b773029b8fa9a4805707043452
KallumBest <kallum.p.best@gmail.com>
2023-10-15 15:55:51 +1300

    updating inventory

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // reference to object we want to turn on and off
+    public GameObject InventoryMenu;
+    //private bool to keep track if the menu is on or off
+    private bool menuActivated;
+
+    //Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if statement to check for input button i
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            // Deactivates Menu
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+
+        // if statement to check for input button i, adding else only executes if previous statement is NOT true
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+            // Activates menu 
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
     }
 }
