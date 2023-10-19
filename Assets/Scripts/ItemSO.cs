27296794b855e0c8585c59159d6d7c71308b17a8
KallumBest <kallum.p.best@gmail.com>
2023-10-19 15:37:30 +1300

    adding health damage and heart pick up

---
+++ Assets/Scripts/ItemSO.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ItemSO;
+
+
+// add create menu allows you to create instances of this sciptable object by clicking Create in Unity
+[CreateAssetMenu]
+public class ItemSO : ScriptableObject
+
+{
+    public string itemName;
+    // player can access variables
+    public StatToChange statToChange = new StatToChange();
+    // amount to change stat
+    public int amountToChangeStat;
+    //enumeration allows you to create drop down menus of related constants
+
+
+    public AttributeToChange attributeToChange = new AttributeToChange();
+    // amount to change stat
+    public int amountToChangeAttribute;
+
+
+
+
+    // how to use item
+
+   
+    public void UseItem()
+   {
+     //  if(statToChange == StatToChange.health)
+     //   {
+     //      GameObject.Find("healthManager").GetComponent<PlayerHealth>().ChangeHealth(amountToChangeStat);
+     //   }
+   }
+
+
+
+    public enum StatToChange
+    {
+        none,
+        health,
+        SoulPoint,
+        attack
+    }; // semicolon to make work
+
+    public enum AttributeToChange
+    {
+        none,
+        strength,
+        defense
+    }; // semicolon to make work
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //keeps track of the players current health
+    public int health;
+    //how much health you have when you are at full health
+    public int maxHealth = 5; // max helath is 5
+
+    public int attack = 1;
+
+    public SpriteRenderer playerSr;
+    public PlayerManager playerManager;
+
+    private void Start()
+    {
+        health = maxHealth;   // start game full health
+    }
+
+    public void TakeDamage(int amount) // function called anytime player takes damage and amount
+    {
+        health -= amount; // amount passed in brackets above
+        if(health <= 0 ) // if the damage takes player to 0 the player is destroyed
+        {
+            playerSr.enabled = false;
+            playerManager.enabled = false;
+        }
+    }
+}
