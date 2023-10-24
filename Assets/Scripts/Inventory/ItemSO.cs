cef00c9a2e10a211dbd63b071d86838df5726b5c
KallumBest <kallum.p.best@gmail.com>
2023-10-24 14:07:42 +1300

    hearts pikced up now restore 1 health per click

--- Assets/Scripts/Inventory/ItemSO.cs
+++ Assets/Scripts/Inventory/ItemSO.cs
@@ -31,7 +31,7 @@ public class ItemSO : ScriptableObject
    {
        if(statToChange == StatToChange.health)
        {
-           GameObject.Find("Heart").GetComponent<PlayerHealth>().ChangeHealth(amountToChangeStat);
+           GameObject.Find("Player").GetComponent<PlayerHealth>().ChangeHealth(amountToChangeStat);
             
         }
    }
--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -34,6 +34,7 @@ public class SkillTree : MonoBehaviour
         }
     }
 
+
     void Start()
         {
 
